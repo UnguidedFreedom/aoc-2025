28f8cc9359d4499562b2ff258b29ef130442c807
Erin Vernier <vernier.benoit@gmail.com>
2025-12-03 09:44:50 +1000

    Day 02 part 2 (mathematical)

--- README.md
+++ README.md
@@ -19,9 +19,9 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
 | [Day 1](./src/bin/01.rs) | `54.3µs` | `54.6µs` |
-| [Day 2](./src/bin/02.rs) | `2.8µs` | `95.3ms` |
+| [Day 2](./src/bin/02.rs) | `2.7µs` | `18.4µs` |
 
-**Total: 95.41ms**
+**Total: 0.13ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,4 +1,6 @@
 use itertools::Itertools;
+use std::cmp::max;
+use std::collections::HashMap;
 
 advent_of_code::solution!(2);
 
@@ -20,7 +22,102 @@ fn split(s: &str) -> (&str, &str) {
 }
 
 fn sum_between(start: u64, end: u64) -> u64 {
-    (end * (end + 1) - start * (start - 1)) / 2
+    if end < start {
+        0
+    } else {
+        (end * (end + 1) - start * (start - 1)) / 2
+    }
+}
+
+fn gen_mult(length: usize, parts: usize) -> u64 {
+    parse(
+        format!(
+            "{}1",
+            format!("1{}", "0".repeat(length - 1)).repeat(parts - 1)
+        )
+        .as_str(),
+    )
+}
+
+fn solve_for_length(start: &str, end: &str, length: usize) -> HashMap<usize, u64> {
+    let (ls, le) = (start.len(), end.len());
+    let (ms, me) = (ls / length, le / length);
+
+    let mut answers: HashMap<usize, u64> = HashMap::new();
+
+    if ls == le {
+        if ls % length != 0 || ls == 1 {
+            return answers;
+        }
+
+        let (sp, ep) = (&start[..length], &end[..length]);
+        let (spu, epu) = (parse(sp), parse(ep));
+        let (sc, ec) = (sp.repeat(ms), ep.repeat(me));
+        let (sc, ec) = (sc.as_str(), ec.as_str());
+        let (scu, ecu) = (parse(sc), parse(ec));
+
+        if sp == ep {
+            if sc >= start && ec <= end {
+                (*answers.entry(ms).or_insert(0)) += scu;
+            }
+        } else {
+            if sc >= start {
+                (*answers.entry(ms).or_insert(0)) += scu;
+            }
+            if ec <= end {
+                (*answers.entry(ms).or_insert(0)) += ecu;
+            }
+
+            let sum = sum_between(spu + 1, epu - 1);
+            let mult = gen_mult(length, ms);
+            (*answers.entry(ms).or_insert(0)) += sum * mult;
+        }
+    } else {
+        if ls % length == 0 && ls > length {
+            let sp = &start[..length];
+            let spu = parse(sp);
+            let sc = sp.repeat(ms);
+            let sc = sc.as_str();
+            let scu = parse(sc);
+
+            if sc >= start {
+                (*answers.entry(ms).or_insert(0)) += scu;
+            }
+
+            let e = pow10(length) - 1;
+
+            let sum = sum_between(spu + 1, e);
+            let mult = gen_mult(length, ms);
+            (*answers.entry(ms).or_insert(0)) += sum * mult;
+        }
+
+        if le % length == 0 {
+            let ep = &end[..length];
+            let epu = parse(ep);
+            let ec = ep.repeat(me);
+            let ec = ec.as_str();
+            let ecu = parse(ec);
+
+            if ec <= end {
+                (*answers.entry(me).or_insert(0)) += ecu;
+            }
+
+            let s = pow10(length - 1);
+
+            let sum = sum_between(s, epu - 1);
+            let mult = gen_mult(length, me);
+            (*answers.entry(me).or_insert(0)) += sum * mult;
+        }
+
+        (max(ms + 1, 2)..=(me - 1)).for_each(|m| {
+            let s = pow10(length - 1);
+            let e = pow10(length) - 1;
+            let sum = sum_between(s, e);
+            let mult = gen_mult(length, m);
+            *answers.entry(m).or_insert(0) += sum * mult;
+        });
+    }
+    answers
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
@@ -101,29 +198,25 @@ pub fn part_two(input: &str) -> Option<u64> {
     let res: u64 = input
         .trim()
         .split(',')
-        .map(|range| {
-            range
-                .split('-')
-                .map(|bound| parse(bound))
-                .collect_tuple::<(u64, u64)>()
-                .unwrap()
-        })
-        .flat_map(|(start, end)| start..=end)
-        .filter(|d| {
-            let ds = d.to_string();
-            let l = ds.len();
+        .map(|range| range.split('-').collect_tuple::<(&str, &str)>().unwrap())
+        .map(|(start, end)| {
+            let mut result = 0;
+            let mut res_map: HashMap<(usize, usize), u64> = HashMap::new();
 
-            (1..=l / 2).any(|period| {
-                if l % period != 0 {
-                    return false;
-                }
+            (1..=(end.len() / 2)).for_each(|length| {
+                let mut possibilities = solve_for_length(start, end, length);
 
-                (0..=l)
-                    .step_by(period)
-                    .tuple_windows()
-                    .map(|(start, end)| &ds[start..end])
-                    .all_equal()
-            })
+                possibilities.iter_mut().for_each(|(m, v)| {
+                    res_map.iter().for_each(|(&(m2, l2), &v2)| {
+                        if m * length == m2 * l2 && length % l2 == 0 {
+                            *v -= v2;
+                        }
+                    });
+                    res_map.insert((*m, length), *v);
+                    result += *v;
+                });
+            });
+            result
         })
         .sum::<u64>();
 
