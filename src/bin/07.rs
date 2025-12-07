607d359717db28259bde94e23df012d3c069b103
Erin Vernier <vernier.benoit@gmail.com>
2025-12-08 09:17:18 +1000

    Optimisation

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -12,16 +12,18 @@ pub fn part_one(input: &str) -> Option<u64> {
 
     lines.fold(HashSet::from([start]), |beams, line| {
         let line = line.collect_vec();
-        beams
-            .into_iter()
-            .flat_map(|beam| match line[beam] {
-                '^' => {
-                    res += 1;
-                    vec![beam - 1, beam + 1]
-                }
-                _ => vec![beam],
-            })
-            .collect()
+        let mut new_beams = HashSet::with_capacity(2 * beams.len());
+        beams.into_iter().for_each(|beam| match line[beam] {
+            '^' => {
+                res += 1;
+                new_beams.insert(beam - 1);
+                new_beams.insert(beam + 1);
+            }
+            _ => {
+                new_beams.insert(beam);
+            }
+        });
+        new_beams
     });
 
     Some(res)
@@ -34,16 +36,19 @@ pub fn part_two(input: &str) -> Option<u64> {
 
     let out = lines.fold(HashMap::from([(start, 1)]), |beams, line| {
         let line = line.collect_vec();
+        let mut new_beams = HashMap::with_capacity(2 * beams.len());
         beams
             .into_iter()
-            .flat_map(|(beam, count)| match line[beam] {
+            .for_each(|(beam, count)| match line[beam] {
                 '^' => {
-                    vec![(beam - 1, count), (beam + 1, count)]
+                    *new_beams.entry(beam - 1).or_default() += count;
+                    *new_beams.entry(beam + 1).or_default() += count;
                 }
-                _ => vec![(beam, count)],
-            })
-            .into_grouping_map()
-            .sum()
+                _ => {
+                    *new_beams.entry(beam).or_default() += count;
+                }
+            });
+        new_beams
     });
 
     let res = out.values().sum();
